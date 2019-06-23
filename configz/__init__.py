f9b7076d6e0ea5ba2af7a4f1d4ea650f4395262e
Joni Turunen <rojun.itu@gmail.com>
2019-06-23 15:04:39 +0300

    preliminary config file and environment variable chaining to configuration

--- configz/__init__.py
+++ configz/__init__.py
@@ -3,6 +3,9 @@
 from typing import NamedTuple, Optional
 from collections import ChainMap
 import inspect
+import os
+import configparser
+from pathlib import Path
 
 __version__ = '0.0.1'
 
@@ -11,11 +14,31 @@ class C(NamedTuple):
     a: str = 'a'
     b: str = 'b'
     x: int = 1
+    y: int = 0
+
+
+def filter_config_fields(d: dict):
+    return {k: v for k, v in d.items() if k in C._fields}
+
+
+def get_config_file(_filepath='test.cfg'):
+    filepath = Path(_filepath)
+    if not filepath.exists():
+        return {}
+
+    config = configparser.ConfigParser()
+    config.read(filepath)
+    return dict(config['Default'])
 
 
 def c(params: Optional[dict] = None, *, __c=[]) -> C:
     if params is not None:
-        __c.append(C(**ChainMap(params, C()._asdict())))
+        __c.append(C(**ChainMap(
+            params,
+            filter_config_fields(get_config_file()),
+            filter_config_fields(os.environ),
+            C()._asdict(),
+        )))
     elif len(__c) == 0:
         __c.append(C())
     return __c[-1]
