e93ddfaf344d5fe9cd11bf44c7d325d46f4982b8
Joni Turunen <rojun.itu@gmail.com>
2019-08-25 20:34:42 +0300

    Better module example

--- examples/module_example/__init__.py
+++ examples/module_example/__init__.py
@@ -1,1 +1,11 @@
+from clima import c, Schema
+
+@c
+class Configuration(Schema):
+    a: str = 'A'  # a description
+    x: int = 1  # x description
+
+
+# Hack to enable autocompletion in IDEs
+c: Configuration = c
 
--- examples/module_example/__main__.py
+++ examples/module_example/__main__.py
@@ -1,25 +1,43 @@
-from clima import c, Schema
-
-
-@c
-class Configuration(Schema):
-    a: str = 'A'  # a description
-    x: int = 1  # x description
-
-
-# Hack to enable autocompletion in IDEs
-c: Configuration = c
+from examples.module_example import c       # Configuration defined in __init__.py
+from examples.module_example import baz     # Using c module wise
 
 
 @c
 class Cli:
     def subcommand_foo(self):
         """This will be shown in --help for subcommand-foo"""
-        print('foo')
-        print(c.a)
-        print(c.x)
+        print('foo: ')
+        baz.print_configuration()
 
     def subcommand_bar(self):
         """This will be shown in --help for subcommand-bar"""
         print('bar')
         print(repr(c))
+
+############################################################
+# $ ./examples/module_example/__main__.py subcommand-foo -- -h
+#
+# Type:        method
+# String form: <bound method Cli.subcommand_foo of <clima.Cli object at 0x000001A6E7C919E8>>
+# File:        C:/Users/foobar/code/py/fissle/examples/module_example/__main__.py
+# Line:        6
+# Docstring:   This will be shown in --help for subcommand-foo
+# Args:
+#     --a (str): a description (Default is 'A')
+#     --x (int): x description (Default is 1)
+#
+# Usage:       __main__.py subcommand-foo [--X ...]
+
+############################################################
+# $ ./examples/module_example/__main__.py subcommand-foo
+# foo:
+# A
+# 1
+
+############################################################
+# $ ./examples/module_example/__main__.py subcommand-foo --a 'amazing' --x 42
+#
+# foo:
+# amazing
+# 42
+
